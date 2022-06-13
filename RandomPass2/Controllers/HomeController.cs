bb9ffb134d9e51552534d81a05d51f920ec2c274
Sean McNeil <smcneil136@gmail.com>
2022-06-13 10:39:08 -0400

    C# Week 2 Day 1 | Algoritm & RandomPass Assignment variation

---
+++ RandomPass2/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RandomPass2.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPass2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // [HttpGet]
+    // public IActionResult GeneratePartial()
+    // {
+
+    //     if (HttpContext.Session.GetInt32("Count") == null)
+    //     {
+    //         HttpContext.Session.SetInt32("Count", 0);
+    //     }
+
+    //     int? start = HttpContext.Session.GetInt32("Count");
+    //     HttpContext.Session.SetInt32("Count", (int)start + 1);
+
+    //     string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    //     char[] stringChars = new char[14];
+    //     var random = new Random();
+
+    //     for (int i = 0; i < stringChars.Length; i++)
+    //     {
+    //         stringChars[i] = chars[random.Next(chars.Length)];
+    //     }
+        
+    //     HttpContext.Session.SetString("RandomPass", new String(stringChars));
+
+    //     return PartialView("PassPartial");
+    // }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ RandomPass2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RandomPass2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
