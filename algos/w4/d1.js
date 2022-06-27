c60b15b50a454b4edaa613b7113b7a3c123f4579
Sean McNeil <smcneil136@gmail.com>
2022-06-27 10:44:28 -0400

    C# Week 4 Day 1 | Algorithm & File Updates

---
+++ algos/w4/d1.js
@@ -0,0 +1,84 @@
+// Stacks
+/* LIFO = Last In First Out */
+
+class Node
+{
+    constructor(value)
+    {
+        this.next=undefined;
+        this.value=value;
+    }
+};
+
+class Stack
+{
+    constructor()
+    {
+        this.top=undefined;
+    }
+
+    Push(value)
+    {
+        //Code me!
+        if(!this.top)
+        {
+            this.top = new Node(value);
+            return this;
+        }
+        let temp = this.top;
+        this.top = new Node(value);
+        this.top.next = temp;
+        return this;
+    }
+
+    Peek()
+    {
+        //Code me!
+        return this.top.value;
+    }
+
+    Pop()
+    {
+        //Code me!
+        if (!this.top){
+            return;
+        }
+        let temp = this.top;
+        this.top = this.top.next;
+        return temp.value;
+    }
+
+    IsEmpty()
+    {
+        //Code me!
+        if (!this.top){
+            return;
+        }
+        return this.top == undefined; 
+    }
+
+    Log()
+    {
+        let str="";
+        for(let node=this.top;node;node=node.next)
+        {
+            str+=node.value+"->";
+        }
+        console.log(str);
+    }
+};
+
+let stack=new Stack();
+console.log(stack.IsEmpty()); /* Expected: true */
+stack.Push(10);
+stack.Push(20);
+stack.Push(30);
+stack.Push(40);
+console.log(stack.IsEmpty()); /* Expected: false */
+stack.Log(); /* Expected: 40->30->20->10-> */
+console.log(stack.Pop()); /* Expected: 40 */
+console.log(stack.Peek()); /* Expected: 30 */
+console.log(stack.Pop()); /* 30 */
+console.log(stack.Pop()); /* 20 */
+console.log(stack.Pop()); /* 10 */
+console.log(stack.IsEmpty()); /* Expected: true */
